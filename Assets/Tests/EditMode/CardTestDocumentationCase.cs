8c81bb0a3847fc365922c3ef38c67c23ff10738f
OkanSHome <okans@gamegos.com>
2025-07-14 01:22:24 +0300

    More unit test added

--- Assets/Tests/EditMode/CardTestDocumentationCase.cs
+++ Assets/Tests/EditMode/CardTestDocumentationCase.cs
@@ -32,5 +32,54 @@ namespace Tests.EditMode
             
             Assert.AreEqual(deadwoodSum, 10);
         }
+        
+        [Test]
+        public void CardSortGroupMeldOnlyTest()
+        {
+            var myCards = new List<MyCard>
+            {
+                new MyCard(1, 'H'), new MyCard(2, 'S'), new MyCard(5, 'D'),
+                new MyCard(4, 'H'), new MyCard(1, 'S'), new MyCard(3, 'D'),
+                new MyCard(4, 'C'), new MyCard(4, 'S'), new MyCard(1, 'D'),
+                new MyCard(3, 'S'), new MyCard(4, 'D')
+            };
+
+            var meldGenerators = new List<IMeldRule>();
+            meldGenerators.Add(new GroupMeldRule());
+
+            var melds = meldGenerators.GenerateAllMelds(myCards);
+            
+            var cardSorter = new DPCardSorter();
+            cardSorter.SortCards(myCards, melds);
+
+            var deadwoodSum = melds.CalculateDeadwoodSum(myCards);
+            
+            Assert.AreEqual(deadwoodSum, 13);
+        }
+        
+        [Test]
+        public void CardSortSmartMeldOnlyTest()
+        {
+            var myCards = new List<MyCard>
+            {
+                new MyCard(1, 'H'), new MyCard(2, 'S'), new MyCard(5, 'D'),
+                new MyCard(4, 'H'), new MyCard(1, 'S'), new MyCard(3, 'D'),
+                new MyCard(4, 'C'), new MyCard(4, 'S'), new MyCard(1, 'D'),
+                new MyCard(3, 'S'), new MyCard(4, 'D')
+            };
+
+            var meldGenerators = new List<IMeldRule>();
+            meldGenerators.Add(new GroupMeldRule());
+            meldGenerators.Add(new RunMeldRule());
+
+            var melds = meldGenerators.GenerateAllMelds(myCards);
+            
+            var cardSorter = new DPCardSorter();
+            cardSorter.SortCards(myCards, melds);
+
+            var deadwoodSum = melds.CalculateDeadwoodSum(myCards);
+            
+            Assert.AreEqual(deadwoodSum, 2);
+        }
     }
 }
