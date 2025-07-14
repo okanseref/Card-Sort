50c978424ebce27ee9c1f5077f85858919d2084f
OkanSHome <okans@gamegos.com>
2025-07-14 11:29:13 +0300

    Singleton and SignalBus added

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    _instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T) + " (Singleton)";
+
+                    // For unit tests
+                    if(Application.isPlaying)
+                        DontDestroyOnLoad(singletonObject);
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
